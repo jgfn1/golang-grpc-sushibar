5d16d9f177815d4eb807d53348bd0d39aebcbfac
César <cesar.silva@neurotech.com.br>
2022-09-04 17:51:33 -0300

    Ajustando cliente para gerar numeros aleatorios diferentes a cada execucao

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"time"
 
 	pb "github.com/afa4/golang-grpc/protos"
 	"google.golang.org/grpc"
@@ -19,6 +20,8 @@ func StartClient() {
 	defer conn.Close()
 	serviceClient := pb.NewIsEvenServiceClient(conn)
 
+	var source = rand.NewSource(time.Now().UnixNano())
+	var rand = rand.New(source)
 	integer := rand.Int31()
 	reply, err := serviceClient.IsEven(context.Background(), &pb.IsEvenRequest{Integer: integer})
 	if err != nil {
